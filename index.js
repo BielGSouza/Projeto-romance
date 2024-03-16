05b59b354735cbf4c79dfb52a6550582097cac67
BielGSouza <gabrielsouzaca05@gmail.com>
2024-03-16 20:36:28 -0300

    botaonao funcionando meia bunda

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ function ClicarS() {
 }
 
 function ClicarN() {
-    var botao = document.getElementById('btnao')
-    botao.style.position = 'relative'
-    var cordenadas = botao.getBoundingClientRect()
-    
+    let btn = document.getElementById('btnao')
+    btn.style.position = 'relative'
+    btn.style.bottom = geraPosiçao(10, 90)
+    btn.style.left = geraPosiçao(10, 90)
+}
+function geraPosiçao(min, max) {
+    return (Math.random() + (max -min) + min) + "%"
 }
