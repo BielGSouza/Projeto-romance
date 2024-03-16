10db10d8e3667d59655864bcd854fc04c3a828a3
BielGSouza <gabrielsouzaca05@gmail.com>
2024-03-15 23:07:25 -0300

    quase tudo pronto, só falta o botão não funcionar

---
+++ index.js
@@ -0,0 +1,10 @@
+function ClicarS() {
+    window.location = 'aceitou.html'
+}
+
+function ClicarN() {
+    var botao = document.getElementById('btnao')
+    botao.style.position = 'relative'
+    var cordenadas = botao.getBoundingClientRect()
+    
+}
